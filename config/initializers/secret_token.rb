9d89145e4dfc052ae63d2b1a26fcc819313df993
Petar Ivacic <petar.ivacic@temple.edu>
2013-08-01 04:49:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wjobs::Application.config.secret_token = 'abf0142afd4aef1176ddee9be8e7668ffe64718155237704818f702fd4a3bb3db1a9543f9e1673ba6e306874eff038cbafe6c2625cb2dafc8a0d79442a38e941'
